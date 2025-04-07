4a54ddcb239f354f848a2030bf852ed2461f5abb
vectorsigmaissomewhere <anishchy501625@gmail.com>
2025-04-07 09:18:46 +0545

    google authentication in auth_app

---
+++ alltypeofauth/alltypeofauth/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('app/',include('auth_app.urls')), 
+
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('accounts/', include('allauth.urls')), 
+    path('logout', LogoutView.as_view()),
+]
